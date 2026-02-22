1e04815043c375ff1034b4913f78d41aa318c55d
TariQ <talsakran@assentify.com>
2026-02-22 04:54:34 +0300

    deletd

--- AssentifySdk/Classes/Flow/AssistedDataEntryStep/EntryTypes/File.swift
+++
@@ -1,5 +0,0 @@
-    private func getIsLocked()->Bool{
-        guard let key = field.inputKey else { return false }
-        let defaultValue = AssistedFormHelper.getDefaultValueValue(key, page, flowController: self.flowController) ?? ""
-        return field.isLocked! && !defaultValue.isEmpty
-    }
