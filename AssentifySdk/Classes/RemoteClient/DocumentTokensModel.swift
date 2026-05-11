316f306ad47bc468bf0b1b703a3c7bb7ef20382b
TariQ <talsakran@assentify.com>
2026-05-11 22:02:44 +0300

    Beta . 3

--- AssentifySdk/Classes/RemoteClient/DocumentTokensModel.swift
+++ AssentifySdk/Classes/RemoteClient/DocumentTokensModel.swift
@@ -13,18 +13,18 @@ public struct DocumentTokensModel:Codable {
 
 
 public struct TokensMappings: Codable {
-    let id: Int
-    let tokenId: Int
-    let aBlockIdentifier: String
-    let stepId: Int
-    let aFlowPropertyIdentifier: String
-    let flowName: String
-    let blockName: String
-    let stepName: String
-    let displayName: String
-    let sourceKey: String
-    let isDeleted: Bool
-    let type: Int
+    public let   id: Int
+    public let   tokenId: Int
+    public let   aBlockIdentifier: String
+    public let   stepId: Int
+    public let   aFlowPropertyIdentifier: String
+    public let   flowName: String
+    public let   blockName: String
+    public let   stepName: String
+    public let   displayName: String
+    public let   sourceKey: String
+    public let   isDeleted: Bool
+    public let   type: Int
 
     enum CodingKeys: String, CodingKey {
         case id
